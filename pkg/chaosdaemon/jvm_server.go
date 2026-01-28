a5dbf16030fad4aad20fd2a770343e6f41db544f
prajithp <prajith.palakkuda@cleartax.in>
2026-01-28 18:57:14 +0530

    fix: run attach command inside the namespace

--- pkg/chaosdaemon/jvm_server.go
+++ pkg/chaosdaemon/jvm_server.go
@@ -82,7 +82,7 @@ func (s *DaemonServer) InstallJVMRules(ctx context.Context,
 			}
 		}
 
-		jars := []string{"byteman.jar", "byteman-helper.jar", "chaos-agent.jar"}
+		jars := []string{"byteman.jar", "byteman-helper.jar", "chaos-agent.jar", "byteman-submit.jar"}
 
 		for _, jar := range jars {
 			source := fmt.Sprintf("%s/lib/%s", bytemanHome, jar)
